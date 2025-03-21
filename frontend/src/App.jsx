7569f974691f4910619fb2ae89acc2f7e468e4fe
fable <pandeyshivanshu2005@gmail.com>
2025-03-21 22:41:37 +0530

    initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-//changes from the laptop
+//changes from the laptop 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
