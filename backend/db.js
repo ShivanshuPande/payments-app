7ba9a4117b99cea1392ecad288d575cae07ee534
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-03-22 12:37:41 +0530

    mongoose changes

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
-const { boolean } = require("zod");
 mongoose.connect("mongodb+srv://Shiv_2005:Shivanshu192005@cluster0.aw2rp.mongodb.net/")
 
 const dbSchema = new mongoose.Schema({
-    firstName : String,
-    lastName : String ,
-    password : String
+    firstName : {
+        type : String ,
+        required  :true
+    },
+    lastName : {
+        type : String,
+        required : true 
+    } ,
+    passWord : {type : String,
+        required : true 
+    }
 })
 
-const User = mongoose.model("User" , dbSchema);
+const User = mongoose.model("Users" , dbSchema);
 
 
 module.exports={
