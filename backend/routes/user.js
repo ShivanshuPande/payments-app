af50d710bcbc5a00e428e49ce4ed758c388cb794
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-03-22 17:11:57 +0530

    add the signin logic

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,14 @@
 const express = require("express") ;
 const zod = require("zod");
-const { User } = require("../db");
+const { User, User } = require("../db");
 const jwt = ("jsonwebtoken")
 const JWT_SECRET = require("../config");
 
 const signSchema = zod.object({
     username : zod.string(),
-    lastname : zod.string(),
     firstName : zod.string(),
-    password  :zod.string()
+    lastname : zod.string(),
+    password  :zod.number()
     
 })
 const router = express.Router();
@@ -33,13 +33,55 @@ router.post("/signup" , async(req,res)=>{
     }
 
 
-    const dbUser = await User.create(body);
+    const dbUser = await User.create({
+        username: req.body.username,
+        password : req.body.password,
+        firstName:req.body.firstName,
+        lastname : req.body.lastname
+    });
     const userId = dbUser._id
     const token = jwt.sign({
         userId
     }, JWT_SECRET)
     res.json({
         message : "User Created successfully",
+        token : token
+    })
+})
+
+router.post("/signin" ,async (req,res)=>{
+
+    const signinSchema = zod.object({
+        username:zod.string(),
+        password : zod.number()
+    })
+
+    const success = signinSchema.safeParse(req.body);
+
+    if(!success){
+        res.json({
+            message: "Please enter Correct inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username : req.body.username,
+        password : req.body.password
+    });
+
+    if(!user){
+        res.status(401).json()({
+            message: "Error logging in please enter correct inputs"
+        })
+    }
+
+    const userId = user._id;
+    const token = jwtsign({
+        userId
+    },JWT_SECRET)
+
+    res.status(200).json({
+        message:"Welcome back to the website",
         token : token
     })
 })
