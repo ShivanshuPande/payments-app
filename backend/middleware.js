1c87364b35c0830fb099f27a4aa8db0b2e848926
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-03-22 17:48:55 +0530

    created the auth -middleware

---
+++ backend/middleware.js
@@ -0,0 +1,36 @@
+const JWT_SECRET = require("./config")
+const jwt = require("jsonwebtoken")
+
+const authMiddleware =()=>{
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(403).json()
+    }
+     
+
+    const token = authHeader.split('')[1];
+    try{
+        const decoded = jwt.verify(token ,JWT_SECRET);
+        
+        if(decoded.userId){
+            req.userId = decoded.userId;
+            next();
+        }else{
+            res.status(403).json({
+                message : "invalid token"
+            })
+        }
+        
+
+    }catch(err){
+        return res.status(403).json({
+            message : "some error"
+        })
+    }
+};
+
+module.exports={
+    authMiddleware
+}
+
