8babe017bb60e527ccd3ffe3262843f1b70d960a
fable <pandeyshivanshu2005@gmail.com>
2025-03-22 13:41:42 +0530

    routes

--- backend/server.js
+++ backend/server.js
@@ -5,4 +5,5 @@ const userRouter = require("./routes/user")
 
 app.use("/api/v1" , mainRouter);
 
-app.use("/api/v1" , userRouter )
+app.use("/api/v1" , userRouter );
+// all the req from this route will be passed to the specific router
