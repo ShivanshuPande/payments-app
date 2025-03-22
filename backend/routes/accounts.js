6230deb1ba59f5d9aca76fd18b63c775c660b076
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-03-23 01:33:19 +0530

    added session in transactions for atomicity

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { authMiddleware } = require("../middleware");
 const { Accounts } = require("../db");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 const app = express();
 router.use(express.json())
@@ -28,23 +29,27 @@ router.get("/balance" ,authMiddleware, async (req,res) => {
 })
 
 router.post("/transfer" , async(req, res)=>{
+    const session = await mongoose.startSession();
+    session.startTransaction();
     const {amount , to} = req.body;
     //transfers and processes like that should be carried out with atomicity-- it has to be completed otherwise the whole process should just role back
     const account = await Accounts.findOne({
         userId : req.userId
-    })
+    }).session(session)
 
     if(account.balance < amount){
-        return res.json({
+        await session.abortTransaction();
+        return res.status(400).json({
             msg : "insufficient balance to processs the request"
         })
     }
 
     const toAccount = await Accounts.findOne({
         userId: to
-    })
+    }).session(session)
 
     if(!toAccount){
+        session.abortTransaction();
         return res.status(400).json({
             msg: "Please a valid account to transfer the money"
         })
@@ -55,14 +60,14 @@ router.post("/transfer" , async(req, res)=>{
     } , {$inc:{
         balance : -amount 
     }
-    })
+    }).session(session);
 
     await Accounts.updateOne({
         userId : to
     } , {$inc: {
         balance : +amount
-    }})
-
+    }}).session(session);
+    await session.commitTransaction();
     res.json({
         msg: "Your Transaction was carried out very smoothly --but with a lot of mistakes"
     })
