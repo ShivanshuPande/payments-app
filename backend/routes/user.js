dbc2221a3e490ddf1199ab8f93e11a820849d9e1
fable <pandeyshivanshu2005@gmail.com>
2025-03-22 14:30:29 +0530

    signup logic

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,47 @@
 const express = require("express") ;
+const zod = require("zod");
+const { User } = require("../db");
+const jwt = ("jsonwebtoken")
+const JWT_SECRET = require("../config");
 
+const signSchema = zod.object({
+    username : zod.string(),
+    password : zod.string(),
+    firstName : zod.string(),
+    password  :zod.string()
+    
+})
 const router = express.Router();
 
-router.get("/" , (req,res)=>{
-    
+router.post("/signup" , async(req,res)=>{
+    const body = req.body;
+    const {success} = signSchema.safeParse(req.body);
+    if(!success){
+        return res.json({
+            message:"Email already taken/ Please enter a valid input"
+        })
+    }
+
+    const user = User.findOne({
+        username : body.username
+    })
+
+    if(user){
+        return res.json({
+            message :"Username already taken /please enter a new one"
+        })
+    }
+
+
+    const dbUser = await User.create(body);
+    const userId = dbUser._id
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+    res.json({
+        message : "User Created successfully",
+        token : token
+    })
 })
 
 module.exports = router;
