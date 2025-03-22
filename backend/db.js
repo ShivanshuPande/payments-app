2424ea6d12f2e31d331bf50e7ac983ba70515495
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-03-22 12:46:19 +0530

    better schema

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,27 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://Shiv_2005:Shivanshu192005@cluster0.aw2rp.mongodb.net/")
+mongoose.connect("mongodb+srv://Shiv_2005:Shivanshu192005@cluster0.aw2rp.mongodb.net/");
 
-const dbSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     firstName : {
         type : String ,
         required  :true
     },
     lastName : {
         type : String,
-        required : true 
+        required : true ,
+        
     } ,
     passWord : {type : String,
-        required : true 
+        required : true ,
+        minLength : 8 
+    } ,
+    email : {
+        type : String ,  
+        required : true
     }
-})
+});
 
-const User = mongoose.model("Users" , dbSchema);
+const User = mongoose.model("Users" , userSchema);
 
 
 module.exports={
