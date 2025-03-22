660644432c6a97095aea65aec02a23e276ade3d6
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-03-22 12:32:59 +0530

    mongoose schema

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,16 @@
 const mongoose = require("mongoose");
+const { boolean } = require("zod");
+mongoose.connect("mongodb+srv://Shiv_2005:Shivanshu192005@cluster0.aw2rp.mongodb.net/")
+
+const dbSchema = new mongoose.Schema({
+    firstName : String,
+    lastName : String ,
+    password : String
+})
+
+const User = mongoose.model("User" , dbSchema);
+
+
+module.exports={
+    User
+}
