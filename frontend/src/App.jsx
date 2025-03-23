c6334786d9adcabf7ec4a6aa3ff2403985f47fc1
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-03-23 18:11:37 +0530

    tailwind better

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,51 @@ import Signup from "./components/pages/Signup";
 import Signin from "./components/pages/Signin";
 import Dashboard from "./components/pages/Dashboard";
 import { Transfer } from "./components/pages/Transfer";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/dashboard" element={<Dashboard />} />
+    //     <Route path="/signup" element={<Signup />} />
+    //     <Route path="/signin" element={<Signin />} />
+    //     <Route path="/transfer" element={<Transfer />} />
+    //   </Routes>
+    // </BrowserRouter>
+
+
     <BrowserRouter>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My App</h1>
+
+      {/* Navigation Buttons */}
+      <Link to="/signup">
+        <button style={buttonStyle}>Go to Signup</button>
+      </Link>
+      <Link to="/signin">
+        <button style={buttonStyle}>Go to Signin</button>
+      </Link>
+
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/transfer" element={<Transfer />} />
       </Routes>
-    </BrowserRouter>
+    </div>
+  </BrowserRouter>
   );
 }
+
+const buttonStyle = {
+  padding: "10px 20px",
+  margin: "10px",
+  backgroundColor: "#4caf50",
+  color: "white",
+  border: "none",
+  borderRadius: "5px",
+  cursor: "pointer",
+  fontSize: "16px",
+};
+
 
 export default App;
